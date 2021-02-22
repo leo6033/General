2fb88c8c1ad26ae200a00268003929fedd7df0a4
leo6033 <long452a@163.com>
2021-02-23 00:41:30 +0800

    Update: create Attack class

---
+++ General/Assets/Scripts/Common/Attack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    private float nextAttackTime;
+
+    public void AttackEnemy(float attackRate)
+    {
+        if(Time.time > nextAttackTime)
+        {
+            nextAttackTime = Time.time + attackRate;
+            // TODO: 伤害判定等
+        }
+    }
+}
