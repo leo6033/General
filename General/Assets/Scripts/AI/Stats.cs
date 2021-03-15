273cc0e4456273dd9e0e1dd596cc4484ae07b952
leo6033 <long452a@163.com>
2021-03-15 17:11:07 +0800

    Update: 添加兵种基本属性

--- General/Assets/Scripts/AI/Stats.cs
+++ General/Assets/Scripts/AI/Stats.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AI/Stats")]
 public class Stats : ScriptableObject
 {
+    [Header("攻击范围")]
     public float attackRange;
+    [Header("攻击间隔时间(攻速)")]
     public float attackRate;
+    [Header("视野距离")]
     public float visionRange;
+    [Header("物攻")]
+    public float physicalAttack;
+    [Header("魔攻")]
+    public float magicAttack;
+    [Header("物防")]
+    public float physicalDefense;
+    [Header("魔防")]
+    public float magicDefense;
+    [Header("移速")]
+    public float moveSpeed;
+    [Header("血量")]
+    public float HP;
+    [Header("暴击")]
+    public float critRate;
+    [Header("暴伤")]
+    public float critHarm;
 }
 
