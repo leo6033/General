e94246fc7d23a6a62d419de897e9c308760d4eb3
leo6033 <long452a@163.com>
2021-03-28 22:47:01 +0800

    Update: 弹道跟踪实现

---
+++ General/Assets/Scripts/Common/shell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shell : MonoBehaviour
+{
+
+    public Transform target;
+
+    private float v;
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        dir.Normalize();
+        Quaternion rotate = Quaternion.LookRotation(dir);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, rotate, 30);
+        transform.position = transform.position + dir * v * Time.deltaTime;
+    }
+}
