e34504647a2e5b7460ea889888af00b197041857
leo6033 <long452a@163.com>
2021-04-04 15:54:50 +0800

    Update: 完成地图绘制

---
+++ General/Assets/Scripts/Menu/MapManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapManager: MonoBehaviour
+{
+    public RectTransform Lines;
+    [Header("起始点")]
+    public List<GameObject> firstPoints;
+
+    [Header("线宽")]
+    public float LineWidth = 3.0f;
+
+    private void Start()
+    {
+        foreach(GameObject point in firstPoints)
+        {
+            Draw(point);
+        }
+    }
+
+    private void Draw(GameObject currentPoint)
+    {
+        PointAttr pointAttr = currentPoint.GetComponent<PointAttr>();
+        foreach(GameObject nextPoint in pointAttr.points)
+        {
+            DrawLine(currentPoint.GetComponent<RectTransform>(), nextPoint.GetComponent<RectTransform>(), Color.red);
+            Draw(nextPoint);
+        }
+        
+    }
+
+    public void DrawLine(RectTransform start, RectTransform target, Color color)
+    {
+        GameObject line = new GameObject("line");
+        line.SetActive(false);
+        RectTransform  lineRT = line.AddComponent<RectTransform>();
+        lineRT.pivot = new Vector2(0, 0.5f);
+        lineRT.localScale = Vector3.one;
+        Image lineImg = line.AddComponent<Image>();
+        lineImg.color = color;
+        lineImg.raycastTarget = false;
+        lineRT.SetParent(Lines);
+        lineRT.position = start.position;
+        lineRT.sizeDelta = Vector2.zero;
+        line.SetActive(true);
+
+        Vector3 targetPos = target.position;
+        Vector3 curPos = start.position;
+        Vector3 dir = targetPos - curPos;
+        lineRT.sizeDelta = new Vector2(dir.magnitude, LineWidth);
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        lineRT.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+
+}
---
+++ General/Assets/Scripts/Menu/PointAttr.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAttr : MonoBehaviour
+{
+    public List<GameObject> points;
+}
