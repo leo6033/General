982c3ae8f0f592a8047866a86341dcc078a2567d
leo6033 <long452a@163.com>
2021-04-27 00:03:24 +0800

    Update: 添加点击进如战斗功能

--- General/Assets/Scripts/Menu/PointAttr.cs
+++ General/Assets/Scripts/Menu/PointAttr.cs
@@ -1,8 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class PointAttr : MonoBehaviour
+public class PointAttr : MonoBehaviour, IPointerClickHandler
 {
     public List<GameObject> points;
+    public string SceneName;
+    public int SceneNum;
+
+    [HideInInspector] public int number;
+    [HideInInspector] public int level;
+    [HideInInspector] public int round;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayerData player = SaveSystem.LoadPlayer();
+        if (player.Rounds == round && player.CurrentLevelLayer == level)
+        {
+            if(SceneName == "")
+            {
+                SceneName = "Level " + player.CurrentLevelLayer + "-" + player.Rounds;
+                if(SceneNum > 1)
+                {
+                    SceneName += "(" + Random.Range(1, SceneNum) + ")";
+                }
+            }
+            player.CuttentPointNum = number;
+            SaveSystem.SavePlayer(player);
+            SceneManager.LoadScene(SceneName);
+        }
+    }
 }
